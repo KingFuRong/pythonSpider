67792148bc4b6c20a90533fd21f7fa97cdc74176
KingFuRong <sr.kingfurong@gmail.com>
2025-07-22 13:56:10 +0800

    Create 吐环境记录.js

---
+++ 逆向百例/吐环境记录.js
@@ -0,0 +1,34 @@
+function get_enviroment(proxy_array) {
+    for(var i=0; i<proxy_array.length; i++){
+        handler = '{\n' +
+            '    get: function(target, property, receiver) {\n' +
+            '        console.log("方法:", "get  ", "对象:", ' +
+            '"' + proxy_array[i] + '" ,' +
+            '"  属性:", property, ' +
+            '"  属性类型:", ' + 'typeof property, ' +
+            // '"  属性值:", ' + 'target[property], ' +
+            '"  属性值类型:", typeof target[property]);\n' +
+            '        return target[property];\n' +
+            '    },\n' +
+            '    set: function(target, property, value, receiver) {\n' +
+            '        console.log("方法:", "set  ", "对象:", ' +
+            '"' + proxy_array[i] + '" ,' +
+            '"  属性:", property, ' +
+            '"  属性类型:", ' + 'typeof property, ' +
+            // '"  属性值:", ' + 'target[property], ' +
+            '"  属性值类型:", typeof target[property]);\n' +
+            '        return Reflect.set(...arguments);\n' +
+            '    }\n' +
+            '}'
+        eval('try{\n' + proxy_array[i] + ';\n'
+        + proxy_array[i] + '=new Proxy(' + proxy_array[i] + ', ' + handler + ')}catch (e) {\n' + proxy_array[i] + '={};\n'
+        + proxy_array[i] + '=new Proxy(' + proxy_array[i] + ', ' + handler + ')}')
+    }
+}
+proxy_array = ['window', 'document', 'location', 'navigator', 'history','screen']
+ 
+ 
+// 这里写你要补的环境
+ 
+ 
+get_enviroment(proxy_array)
